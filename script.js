9ef0b2d9d0a46cd06dad0efb906cb85453cede47
Jaszium Styrx Paralex <58339115+jaszium@users.noreply.github.com>
2020-01-09 21:52:18 +0800

    Uploaded a .js & a .css file
    Added custom JavaScript scripting into "script.js"
    Added "w3.css" - unmodified from "https://www.w3schools.com/w3css/4/w3.css"

---
+++ script.js
@@ -0,0 +1,8 @@
+function w3_open(){
+	//document.getElementById("ʒoverlay").style.display = "block";
+	document.getElementById("ʒsidebar").style.display = "block";
+}
+function w3_close(){
+	//document.getElementById("ʒoverlay").style.display = "none";
+	document.getElementById("ʒsidebar").style.display = "none";
+}
