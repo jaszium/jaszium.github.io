485668bc44c1277b20572c74658172c1815dcf98
Jaszium Styrx Paralex <58339115+jaszium@users.noreply.github.com>
2020-01-10 22:26:55 +0800

    Modified the script.js.
    First two functions should be unchanged.
    Added 3 functions:
    1) To prompt for user [name]; and call #2 if [name].length > 21; else its "guest" if empty / null, returns [name]
    2) To confirm if want to type shorter name (nickname / alias) by call #1, or return "guest"
    3) To call #1 when html <body onload="#3()"> has finished loading from index.html;
    It then equates the return of the call #1 to the innerHTML of an element with id: "namen".

--- script.js
+++ script.js
@@ -6,3 +6,33 @@ function w3_close(){
 	//document.getElementById("ʒoverlay").style.display = "none";
 	document.getElementById("ʒsidebar").style.display = "none";
 }
+function ʒguest_name(){
+	var alias;
+	//var person = prompt("Please enter your name:", "Harry Potter");
+	var person = prompt("What is your name?");
+	if (person == null || person == "") {
+		alias = "guest";
+	}
+	else if (person.length > 21) {
+		alias = ʒname_toolong();
+	}
+	else {
+		alias = person;
+	}
+	return alias;
+}
+function ʒname_toolong() {
+	var names;
+	var choice = confirm("Your name is too long! Maximum is 21 letters.\n"+
+	"Press OK to type your nickname, or Cancel to be known as a \"guest\".")
+	if (choice) {
+		names = ʒguest_name();
+	}
+	else {
+		names = "guest";
+	}
+	return names;
+}
+function ʒnamen() {
+	document.getElementById("namen").innerHTML = ʒguest_name();
+}
