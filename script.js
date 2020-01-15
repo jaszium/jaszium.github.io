46b526a9a5b05625bd8dae5fc8c1400eba926fdd
Jaszium Styrx Paralex <58339115+jaszium@users.noreply.github.com>
2020-01-15 14:21:28 +0800

    Updated the script.js
    Added js date & time counters
    Slowed loading time of page due to high refresh interval (reloads every 0.5 sec)

--- script.js
+++ script.js
@@ -36,3 +36,46 @@ function ʒname_toolong() {
 function ʒnamen() {
 	document.getElementById("namen").innerHTML = ʒguest_name();
 }
+function unix(x) {
+	ticks = setInterval(counter, x);
+}
+function pause() {
+	clearInterval(ticks);
+}
+function counter() {
+	var d = new Date();
+	var l = Date.now().toString();
+	var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+	document.getElementById("lclUnx").innerHTML = l[0] + ", " + l[1] + l[2] + l[3] + ", " + 
+	l[4] + l[5] + l[6] + ", " + l[7] + l[8] + l[9] + "." + l[10] + l[11] + l[12];
+	document.getElementById("lclYər").innerHTML = d.getFullYear();
+	document.getElementById("lclMth").innerHTML = d.getMonth() + " (" + months[d.getMonth()] + ")";
+	document.getElementById("lclDay").innerHTML = zeropad(d.getDate());
+	document.getElementById("lclHør").innerHTML = zeropad(d.getHours());
+	document.getElementById("lclMin").innerHTML = zeropad(d.getMinutes());
+	document.getElementById("lclSec").innerHTML = zeropad(d.getSeconds());
+	document.getElementById("lclMil").innerHTML = msecpad(d.getMilliseconds());
+	document.getElementById("lclTzN").innerHTML = Intl.DateTimeFormat().resolvedOptions().timeZone;
+	document.getElementById("lclTzI").innerHTML = d.getTimezoneOffset()/60 + ":00";
+	var u = (Date.now()+(d.getTimezoneOffset()*60*1000)).toString();
+	document.getElementById("utcUnx").innerHTML = u[0] + ", " + u[1] + u[2] + u[3] + ", " + 
+	u[4] + u[5] + u[6] + ", " + u[7] + u[8] + u[9] + "." + u[10] + u[11] + u[12];
+	document.getElementById("utcYər").innerHTML = d.getUTCFullYear();
+	document.getElementById("utcMth").innerHTML = d.getUTCMonth() + " (" + months[d.getUTCMonth()] + ")";
+	document.getElementById("utcDay").innerHTML = zeropad(d.getUTCDate());
+	document.getElementById("utcHør").innerHTML = zeropad(d.getUTCHours());
+	document.getElementById("utcMin").innerHTML = zeropad(d.getUTCMinutes());
+	document.getElementById("utcSec").innerHTML = zeropad(d.getUTCSeconds());
+	document.getElementById("utcMil").innerHTML = msecpad(d.getUTCMilliseconds());
+	document.getElementById("utcTzN").innerHTML = Intl.DateTimeFormat().resolvedOptions().timeZone;
+	document.getElementById("utcTzI").innerHTML = d.getTimezoneOffset()/60 + ":00";
+}
+function zeropad(i) {
+	if (i < 10) {i = "0" + i};
+	return i;
+}
+function msecpad(i) {
+	if (i < 100) {i = "0" + i}
+	else if (i < 10) {i = "00" + i};
+	return i;
+}
