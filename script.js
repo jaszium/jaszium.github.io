23276c6e6dc409dfcd983528eafdf40064d2b689
Jaszium Styrx Paralex <58339115+jaszium@users.noreply.github.com>
2020-01-17 22:55:18 +0800

    Updated the script.js, custom ʒfunctions with UTC
    Added " ʒ " to most function & vars;
    Added " UTC " in the timezone column of UTC row;
    Added " ±0:00 " as timezone offset for UTC;
    Removed extra whitespace, compacted code length a bit.

--- script.js
+++ script.js
@@ -8,73 +8,71 @@ function w3_close(){
 }
 function ʒguest_name(){
 	var alias;
-	//var person = prompt("Please enter your name:", "Harry Potter");
+	//var person = prompt("Please enter your name:", "Jaszium Styrx");
 	var person = prompt("What is your name?");
-	if (person == null || person == "") {
-		alias = "guest";
-	}
-	else if (person.length > 22) {
-		alias = ʒname_toolong();
-	}
-	else {
-		alias = person;
-	}
+	if (person == null || person == "") {alias = "guest"}
+	else if (person.length > 22) {alias = ʒname_toolong()}
+	else {alias = person};
 	return alias;
 }
 function ʒname_toolong() {
 	var names;
 	var choice = confirm("Your name is too long! Maximum is 22 letters.\n"+
 	"Press OK to type your nickname, or Cancel to be known as a \"guest\".")
-	if (choice) {
-		names = ʒguest_name();
-	}
-	else {
-		names = "guest";
-	}
+	if (choice) {names = ʒguest_name()}
+	else {names = "guest"};
 	return names;
 }
 function ʒnamen() {
 	document.getElementById("namen").innerHTML = ʒguest_name();
 }
-function unix(x) {
-	ticks = setInterval(counter, x);
+function ʒçroǹsync(x) {
+	ʒruntime = setInterval(ʒchronos, x);
 }
-function pause() {
-	clearInterval(ticks);
+function ʒçroǹwait() {
+	clearInterval(ʒruntime);
 }
-function counter() {
+function ʒchronos() {
 	var d = new Date();
-	var l = Date.now().toString();
 	var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-	document.getElementById("lclUnx").innerHTML = l[0] + ", " + l[1] + l[2] + l[3] + ", " + 
-	l[4] + l[5] + l[6] + ", " + l[7] + l[8] + l[9] + "." + l[10] + l[11] + l[12];
+	var greenwich = new Intl.DateTimeFormat(Intl.DateTimeFormat().resolvedOptions().locale, { timeZone: 'UTC' });
+	
+	var l = Date.now().toString();
+	document.getElementById("lclUnx").innerHTML = l[0] + ", " + l[1] + l[2] + l[3] + 
+	", " + l[4] + l[5] + l[6] + ", " + l[7] + l[8] + l[9] + "." + l[10] + l[11] + l[12];
 	document.getElementById("lclYər").innerHTML = d.getFullYear();
 	document.getElementById("lclMth").innerHTML = d.getMonth() + " / " + months[d.getMonth()];
-	document.getElementById("lclDay").innerHTML = zeropad(d.getDate());
-	document.getElementById("lclHør").innerHTML = zeropad(d.getHours());
-	document.getElementById("lclMin").innerHTML = zeropad(d.getMinutes());
-	document.getElementById("lclSec").innerHTML = zeropad(d.getSeconds());
-	document.getElementById("lclMil").innerHTML = msecpad(d.getMilliseconds());
+	document.getElementById("lclDay").innerHTML = ʒzeropad(d.getDate());
+	document.getElementById("lclHør").innerHTML = ʒzeropad(d.getHours());
+	document.getElementById("lclMin").innerHTML = ʒzeropad(d.getMinutes());
+	document.getElementById("lclSec").innerHTML = ʒzeropad(d.getSeconds());
+	document.getElementById("lclMil").innerHTML = ʒmsecpad(d.getMilliseconds());
 	document.getElementById("lclTzN").innerHTML = Intl.DateTimeFormat().resolvedOptions().timeZone;
 	document.getElementById("lclTzI").innerHTML = d.getTimezoneOffset()/60 + ":00";
+	
 	var u = (Date.now()+(d.getTimezoneOffset()*60*1000)).toString();
-	document.getElementById("utcUnx").innerHTML = u[0] + ", " + u[1] + u[2] + u[3] + ", " + 
-	u[4] + u[5] + u[6] + ", " + u[7] + u[8] + u[9] + "." + u[10] + u[11] + u[12];
+	document.getElementById("utcUnx").innerHTML = u[0] + ", " + u[1] + u[2] + u[3] + 
+	", " + u[4] + u[5] + u[6] + ", " + u[7] + u[8] + u[9] + "." + u[10] + u[11] + u[12];
 	document.getElementById("utcYər").innerHTML = d.getUTCFullYear();
 	document.getElementById("utcMth").innerHTML = d.getUTCMonth() + " / " + months[d.getUTCMonth()];
-	document.getElementById("utcDay").innerHTML = zeropad(d.getUTCDate());
-	document.getElementById("utcHør").innerHTML = zeropad(d.getUTCHours());
-	document.getElementById("utcMin").innerHTML = zeropad(d.getUTCMinutes());
-	document.getElementById("utcSec").innerHTML = zeropad(d.getUTCSeconds());
-	document.getElementById("utcMil").innerHTML = msecpad(d.getUTCMilliseconds());
-	document.getElementById("utcTzN").innerHTML = Intl.DateTimeFormat().resolvedOptions().timeZone;
-	document.getElementById("utcTzI").innerHTML = d.getTimezoneOffset()/60 + ":00";
+	document.getElementById("utcDay").innerHTML = ʒzeropad(d.getUTCDate());
+	document.getElementById("utcHør").innerHTML = ʒzeropad(d.getUTCHours());
+	document.getElementById("utcMin").innerHTML = ʒzeropad(d.getUTCMinutes());
+	document.getElementById("utcSec").innerHTML = ʒzeropad(d.getUTCSeconds());
+	document.getElementById("utcMil").innerHTML = ʒmsecpad(d.getUTCMilliseconds());
+	document.getElementById("utcTzN").innerHTML = greenwich.resolvedOptions().timeZone;
+	document.getElementById("utcTzI").innerHTML = "±" + (d.getTimezoneOffset()-d.getTimezoneOffset())/60 + ":00";
+	
+	//document.getElementById("utcTzN").innerHTML = Intl.DateTimeFormat().resolvedOptions().locale; //en-GB
+	//document.getElementById("utcTzN").innerHTML = Intl.DateTimeFormat().resolvedOptions().calendar; //gregory
+	//document.getElementById("utcTzN").innerHTML = Intl.DateTimeFormat().resolvedOptions().numberingSystem; //latn
+	//document.getElementById("utcTzN").innerHTML = Intl.DateTimeFormat().resolvedOptions().timeZone; //Asia/Kuala_Lumpur
 }
-function zeropad(i) {
+function ʒzeropad(i) {
 	if (i < 10) {i = "0" + i};
 	return i;
 }
-function msecpad(i) {
+function ʒmsecpad(i) {
 	if (i < 100) {i = "0" + i}
 	else if (i < 10) {i = "00" + i};
 	return i;
